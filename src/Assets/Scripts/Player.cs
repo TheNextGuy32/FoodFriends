1e353206815f52fbef5f6bd0f064c97dc6c6a9d9
dennisc10001 <Dennis@eduroam-79-147.uni-paderborn.de>
2014-05-29 19:19:55 +0200

    player class
    player class

---
+++ src/Assets/Scripts/Player.cs
@@ -0,0 +1,41 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class player : MonoBehaviour {
+
+	//Score of current Player
+	private int _score;
+
+	public int score{
+
+		get{ return _score;}
+		set{ _score = value;}
+
+	}
+	//Timer of current Player
+	private Time _timer;
+
+
+
+	//Name of current Player
+	private string _name;
+
+	public string name{
+
+		get{ return _name;}
+		set{ _name = value;}
+
+	}
+
+
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
