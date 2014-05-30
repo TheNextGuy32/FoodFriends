d551425e935fc605a3cc6728ce405b40d300b1b9
dennisc10001 <Dennis@eduroam-79-147.uni-paderborn.de>
2014-05-30 21:56:07 +0200

    Funktional Scoring!

--- src/js/main.js
+++ src/js/main.js
@@ -303,10 +303,11 @@ app.main = {
 
             this.changeCountry(active, notActive);
         }
+        
+        this.showScore();
 
         //draw countries
-		var Fort_Worth_TX = 0.76422;
-        var sizeOfCountry = app.dimensions.width / 10;
+		
         //app.ctx.drawImage(this.activeCountryArray[0].getImage(), app.dimensions.width / 4 - sizeOfCountry / 2, app.dimensions.height / 10, sizeOfCountry, sizeOfCountry * Fort_Worth_TX);
 		app.ctx.drawImage(this.activeCountryArray[0].getImage(), app.main.DEFAULT_WIDTH / 6, 100, 32, 32);
 		app.ctx.drawImage(this.activeCountryArray[1].getImage(), app.main.DEFAULT_WIDTH / 6 * 3, 100, 32, 32);
@@ -323,9 +324,9 @@ app.main = {
                               this.foodSize * app.dimensions.scale, this.foodSize * app.dimensions.scale);*/
 			app.ctx.drawImage(this.foods[f].image, this.foods[f].x, this.foods[f].y, this.foodSize, this.foodSize);
             
+            this.checkfood(this.foods[f], this.activeCountryArray[0]);
             
             
-            this.showScore();
         }
     },
 
@@ -336,7 +337,6 @@ app.main = {
       **/
     showScore: function () {
         // calculate size of font based on screen dimension
-        var size;
        
         this.font = '10px sans-serif';
         app.ctx.fillStyle = "#000000";
@@ -344,7 +344,7 @@ app.main = {
         //app.ctx.textBaseline = 'bottom';
         //app.ctx.lineWidth = 1;
         app.ctx.fillText("Score: " + app.player.getScore() +
-                " " + app.player.getName(), 20,40);
+                " " + app.player.getName(), 10,10);
     },
 
     switchLane: function (button) {
@@ -366,10 +366,13 @@ app.main = {
         this.activeCountryArray[active] = this.notActiveCountryArray[notActive];
         this.notActiveCountryArray[notActive] = tmpCountry;
     },
+  
     checkfood:function(food,country){
     	
-    	if(food.country == country && food.y>(4/5)*app.dimensions.height)
-    		app.player.inkScore();
-    	
+    	if(food.country !== country.countryName && food.y>100 && food.y<102)
+    		{app.player.inkScore();}
+    	if(food.country == country.countryName && food.y>100 && food.y<102)
+    		{app.player.decScore();
+    		console.log("DECREASE");}
     }
 };
