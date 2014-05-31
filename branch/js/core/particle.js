1d677a8bf5e5088aa29cd609e63b4ee14ee4e1c3
Freddy Garcia <fsg7957@rit.edu>
2014-05-31 09:36:04 -0400

    Branch: emitter and particle now included!

---
+++ branch/js/core/particle.js
@@ -0,0 +1,78 @@
+//particle.js
+//Dependencies: vector.js
+//Elvis Perez
+"use strict"
+
+var Particle = function(pos, vel, accel, lifetime, size, colour, toFade)
+{
+	this.position = pos;
+	this.velocity = vel;
+	this.acceleration = accel;
+	
+	this.age = lifetime;
+	this.radius = size;
+	this.color = colour;
+	
+	this.lifespan = lifetime;
+	this.fade = toFade;
+	this.active = true;
+};
+
+//Moves the particle based on its movement vectors
+Particle.prototype.move = function()
+{
+	//Euler Integration
+	
+	//Velocity += Acceleration
+	this.velocity.add(this.acceleration);
+	
+	//Position += Velocity
+	this.position.add(this.velocity);
+};
+
+//Reduce the age of the particle
+Particle.prototype.decay = function()
+{
+	///May not want to make lifetime framebased
+	this.age--; //Reduce age
+	
+	//Set particle inactive if exceeds lifetime
+	if(this.age <= 0)
+	{
+		this.age = 0; //Prevent alpha value based on age from becoming negative
+		this.active = false;
+	}
+};
+
+//Update the properties of the particle
+Particle.prototype.update = function()
+{
+	//Only act on the particle if it is active
+	if(this.active)
+	{
+		this.move();
+		this.decay();
+		
+		if(this.fade)
+		{
+			//Linear fade
+			var arr = this.color.split(",");
+			this.color = arr[0] + "," + arr[1] + ", " + arr[2] + "," + this.age / this.lifespan + ")";
+		}
+	}
+};
+
+Particle.prototype.draw = function(ctx)
+{
+	ctx.save();
+
+	ctx.fillStyle = this.color;
+	
+	//Circular particles
+	///Possible upgrade: Allow shape customization
+	ctx.beginPath();
+	ctx.arc(this.position.x, this.position.y, this.radius, 0, Math.PI * 2);
+	ctx.fill();
+	
+	ctx.restore();
+};
